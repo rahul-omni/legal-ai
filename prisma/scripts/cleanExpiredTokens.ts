361d42ae5d57b17f3a51c9720cea53f934da3dec
anil-arthmate <anil.kumar@arthmate.co.in>
2025-04-28 18:05:44 +0530

    Add node-cron dependency and implement script to clean expired verification tokens daily

---
+++ prisma/scripts/cleanExpiredTokens.ts
@@ -0,0 +1,19 @@
+import { logger } from "@/app/api/lib/logger";
+import { db } from "@/lib/db";
+import { schedule } from "node-cron";
+
+// Run daily at 3 AM
+schedule("0 3 * * *", async () => {
+  await db.user.updateMany({
+    where: {
+      verificationTokenExpiry: {
+        lt: new Date(),
+      },
+    },
+    data: {
+      verificationToken: null,
+      verificationTokenExpiry: null,
+    },
+  });
+  logger.info("Cleaned expired verification tokens");
+});
