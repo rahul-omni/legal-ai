24db71d146553f7dd3e0b230cc32a81b605b773d
anil-arthmate <anil.kumar@arthmate.co.in>
2025-05-19 19:22:32 +0530

    refactor: remove unused components and related files from the project

--- src/app/billing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BillGenerator } from '@/components/BillGenerator';
-
-export default function BillingPage() {
-  return (
-    <div className="h-screen flex flex-col">
-      <BillGenerator />
-    </div>
-  );
-} 
