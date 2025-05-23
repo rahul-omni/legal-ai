6687ed38ec2ce12185a8bce3643b41a98d1716c8
anil-arthmate <anil.kumar@arthmate.co.in>
2025-05-23 19:21:14 +0530

    feat: add EditorInitializer and ToolbarPlugin components with initial configuration

--- src/components/LegalEditor/components/DocumentEditor.tsx
+++ src/components/LegalEditor/components/DocumentEditor.tsx
@@ -1,40 +1,21 @@
-import { $generateNodesFromDOM } from "@lexical/html";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
-import { mergeRegister } from "@lexical/utils";
 import {
   $createTextNode,
   $getRoot,
   $getSelection,
-  $isRangeSelection,
-  CAN_REDO_COMMAND,
-  CAN_UNDO_COMMAND,
   EditorState,
-  FORMAT_ELEMENT_COMMAND,
-  FORMAT_TEXT_COMMAND,
   LexicalEditor,
-  REDO_COMMAND,
-  SELECTION_CHANGE_COMMAND,
-  UNDO_COMMAND,
 } from "lexical";
-import {
-  AlignCenter,
-  AlignJustify,
-  AlignLeft,
-  AlignRight,
-  Bold,
-  Italic,
-  Redo2,
-  Strikethrough,
-  Underline,
-  Undo2,
-} from "lucide-react";
-import { FC, RefObject, useCallback, useEffect, useRef, useState } from "react";
+import { FC, RefObject, useRef } from "react";
+import { ToolbarPlugin } from "./lexical/ToolbarPlugin";
+import { initialConfig } from "./lexical/initalConfig";
+import { EditorInitializer } from "./lexical/EditorInitializer";
+import "./lexical/lexical.css";
 
 interface DocumentEditorProps {
   localContent: string;
@@ -52,16 +33,6 @@ export const DocumentEditor: FC<DocumentEditorProps> = ({
   const caretPositionRef = useRef<{ index: number; length: number } | null>(
     null
   );
-
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme: {},
-    onError,
-  };
-
-  function onError(error: any) {
-    console.log("Editor error:", error);
-  }
 
   const handleEditorSelectionChange = (editorState: EditorState) => {
     let text: string | undefined;
@@ -98,7 +69,10 @@ export const DocumentEditor: FC<DocumentEditorProps> = ({
       <ToolbarPlugin />
       <div className="editor-inner">
         <RichTextPlugin
-          contentEditable={<ContentEditable className="content-editable" />}
+          placeholder={
+            <div className="editor-placeholder">enter text here...</div>
+          }
+          contentEditable={<ContentEditable className="editor-input" />}
           ErrorBoundary={LexicalErrorBoundary}
         />
         <OnChangePlugin onChange={handleEditorSelectionChange} />
@@ -126,190 +100,4 @@ export function insertTextAtSelection(editor: LexicalEditor, text: string) {
       }
     }
   });
-}
-
-function EditorInitializer({
-  localContent,
-  setEditorRef,
-}: {
-  localContent: string;
-  setEditorRef: (_editor: LexicalEditor) => void;
-}) {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    setEditorRef(editor);
-
-    if (!localContent) return;
-    editor.update(() => {
-      const parser = new DOMParser();
-      const dom = parser.parseFromString(localContent, "text/html");
-      const nodes = $generateNodesFromDOM(editor, dom);
-      const root = $getRoot();
-      root.clear();
-      root.append(...nodes);
-    });
-  }, [editor, localContent]);
-
-  return null;
-}
-
-const LowPriority = 1;
-
-function Divider() {
-  return <div className="divider" />;
-}
-
-export default function ToolbarPlugin() {
-  const [editor] = useLexicalComposerContext();
-  const toolbarRef = useRef(null);
-  const [canUndo, setCanUndo] = useState(false);
-  const [canRedo, setCanRedo] = useState(false);
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
-  const [isUnderline, setIsUnderline] = useState(false);
-  const [isStrikethrough, setIsStrikethrough] = useState(false);
-
-  const $updateToolbar = useCallback(() => {
-    const selection = $getSelection();
-    if ($isRangeSelection(selection)) {
-      setIsBold(selection.hasFormat("bold"));
-      setIsItalic(selection.hasFormat("italic"));
-      setIsUnderline(selection.hasFormat("underline"));
-      setIsStrikethrough(selection.hasFormat("strikethrough"));
-    }
-  }, []);
-
-  useEffect(() => {
-    return mergeRegister(
-      editor.registerUpdateListener(({ editorState }) => {
-        editorState.read(() => {
-          $updateToolbar();
-        });
-      }),
-      editor.registerCommand(
-        SELECTION_CHANGE_COMMAND,
-        (_payload, _newEditor) => {
-          $updateToolbar();
-          return false;
-        },
-        LowPriority
-      ),
-      editor.registerCommand(
-        CAN_UNDO_COMMAND,
-        (payload) => {
-          setCanUndo(payload);
-          return false;
-        },
-        LowPriority
-      ),
-      editor.registerCommand(
-        CAN_REDO_COMMAND,
-        (payload) => {
-          setCanRedo(payload);
-          return false;
-        },
-        LowPriority
-      )
-    );
-  }, [editor, $updateToolbar]);
-
-  return (
-    <div className="toolbar" ref={toolbarRef}>
-      <button
-        disabled={!canUndo}
-        onClick={() => {
-          editor.dispatchCommand(UNDO_COMMAND, undefined);
-        }}
-        className="toolbar-item spaced"
-        aria-label="Undo"
-      >
-        <Undo2 size={18} />
-      </button>
-      <button
-        disabled={!canRedo}
-        onClick={() => {
-          editor.dispatchCommand(REDO_COMMAND, undefined);
-        }}
-        className="toolbar-item"
-        aria-label="Redo"
-      >
-        <Redo2 size={18} />
-      </button>
-      <Divider />
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
-        }}
-        className={"toolbar-item spaced " + (isBold ? "active" : "")}
-        aria-label="Format Bold"
-      >
-        <Bold size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
-        }}
-        className={"toolbar-item spaced " + (isItalic ? "active" : "")}
-        aria-label="Format Italics"
-      >
-        <Italic size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
-        }}
-        className={"toolbar-item spaced " + (isUnderline ? "active" : "")}
-        aria-label="Format Underline"
-      >
-        <Underline size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
-        }}
-        className={"toolbar-item spaced " + (isStrikethrough ? "active" : "")}
-        aria-label="Format Strikethrough"
-      >
-        <Strikethrough size={18} />
-      </button>
-      <Divider />
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "left");
-        }}
-        className="toolbar-item spaced"
-        aria-label="Left Align"
-      >
-        <AlignLeft size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "center");
-        }}
-        className="toolbar-item spaced"
-        aria-label="Center Align"
-      >
-        <AlignCenter size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "right");
-        }}
-        className="toolbar-item spaced"
-        aria-label="Right Align"
-      >
-        <AlignRight size={18} />
-      </button>
-      <button
-        onClick={() => {
-          editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, "justify");
-        }}
-        className="toolbar-item"
-        aria-label="Justify Align"
-      >
-        <AlignJustify size={18} />
-      </button>{" "}
-    </div>
-  );
 }
---
+++ src/components/LegalEditor/components/lexical/EditorInitializer.tsx
@@ -0,0 +1,30 @@
+import { $generateNodesFromDOM } from "@lexical/html";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { $getRoot, LexicalEditor } from "lexical";
+import { useEffect } from "react";
+
+export function EditorInitializer({
+  localContent,
+  setEditorRef,
+}: {
+  localContent: string;
+  setEditorRef: (_editor: LexicalEditor) => void;
+}) {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    setEditorRef(editor);
+
+    if (!localContent) return;
+    editor.update(() => {
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(localContent, "text/html");
+      const nodes = $generateNodesFromDOM(editor, dom);
+      const root = $getRoot();
+      root.clear();
+      root.append(...nodes);
+    });
+  }, [editor, localContent]);
+
+  return null;
+}
