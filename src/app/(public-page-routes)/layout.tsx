1aacc61a878dd6df06b2ec80a1f1aaab56edd91f
anil-arthmate <anil.kumar@arthmate.co.in>
2025-05-28 15:32:15 +0530

    Adds loading state to public layout

--- src/app/(public-page-routes)/layout.tsx
+++ src/app/(public-page-routes)/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import React from "react";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -12,8 +12,14 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          Loading...
+        </div>
+      }
+    >
       {children}
-    </>
+    </Suspense>
   );
 }
