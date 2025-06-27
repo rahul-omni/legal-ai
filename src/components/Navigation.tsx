dab6cdd3130ec7a37a84afc7b618d6722ec5490e
rawatkash <akash.rawat@arthmate.co.in>
2025-06-27 16:04:52 +0530

    show tooltip on collapse

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -86,7 +86,7 @@ export function Navigation() {
             <Link
               key={item.name}
               href={item.href}
-              title={item.name}
+              title={isExpanded ? "" : item.name}
               className={`
                 relative flex items-center gap-3 px-4 py-2 mx-2 rounded-md transition-colors
                 ${isExpanded ? "" : "justify-center"} 
